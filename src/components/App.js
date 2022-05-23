b91a27e61be9d2e6833479c8cf446426ab18988d
IvanMonichev <monichev@webelement.ru>
2022-05-23 18:02:40 +0300

    feat: add PopupWithForm

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,54 @@ import ImagePopup from "./ImagePopup";
 
 function App() {
   return (
-      <div>
+      <>
         <Header />
         <Main />
         <Footer/>
-        <PopupWithForm />
-        <ImagePopup />
+        <PopupWithForm
+          name="edit-avatar"
+          title="Обновить аватар"
+          children={
+              <>
+                <input type="url"
+                       id="avatar-input"
+                       className="popup__text-input
+                        popup__text-input_type_avatar"
+                     name="avatar"
+                     placeholder="Ссылка на аватар"
+                       required />
+                  <span className="popup__input-error avatar-input-error">Ошибка</span>
+                  <button className="popup__save-button button-action">Сохранить</button>
+                  <button type="button" className="popup__close-button button-action"></button>
+              </>
+          } />
+        <PopupWithForm
+        name="edit-profile"
+        title="Редактировать профиль"
+        children={
+            <>
+                <input type="text"
+                       id="name-input"
+                       className="popup__text-input popup__text-input_type_name"
+                       name="name"
+                       placeholder="Имя"
+                       required minLength="2"
+                       maxLength="40" />
+                <span className="popup__input-error name-input-error">Ошибка</span>
+                <input type="text"
+                       id="about-input"
+                       className="popup__text-input
+                       popup__text-input_type_about"
+                       name="about"
+                       placeholder="О себе"
+                       required minLength="2"
+                       maxLength="200" />
+                <span className="popup__input-error about-input-error">&nbsp;</span>
+                <button type="submit" className="popup__save-button button-action">Сохранить</button>
+                <button type="button" className="popup__close-button button-action"></button>
+            </>
+        } />
+
 
         <template className="photo-gallery__item-template">
           <li className="photo-gallery__item">
@@ -26,7 +68,7 @@ function App() {
           </li>
         </template>
 
-      </div>
+      </>
   );
 }
 
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,8 +5,6 @@ function PopupWithForm(props) {
             <form className="popup__container" name={props.name} noValidate>
                 <h2 className="popup__title">{props.title}</h2>
                 {props.children}
-                <button type="submit" className="popup__save-button button-action">Сохранить</button>
-                <button type="button" className="popup__close-button button-action"></button>
             </form>
         </section>
 
