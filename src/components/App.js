ae25239e4cc277eb1f3ee2af35f1d764a7393175
Ivan Monichev <ivan@monichev.ru>
2022-05-24 01:29:48 +0300

    feat: add cards

--- src/components/App.js
+++ src/components/App.js
@@ -114,16 +114,7 @@ function App() {
 
         <ImagePopup />
         <template className="photo-gallery__item-template">
-          <li className="photo-gallery__item">
-              <img src="../images" alt="Фотография загруженная пользователем «{title}»" className="photo-gallery__image" />
-                  <h2 className="photo-gallery__title"></h2>
-                  <div className="photo-gallery__like-section">
-                      <button className="photo-gallery__like-button button-action"></button>
-                      <p className="photo-gallery__like-counter">0</p>
-                  </div>
-                  <a className="photo-gallery__delete-button button-action"><img
-                      src="/" alt="Корзина удаления" /></a>
-          </li>
+
         </template>
 
       </>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,21 @@
 import React from "react";
 import api from '../utils/Api'
+import trash from '../images/trash.svg';
 
 function Main(props) {
 
   const [userName, setUserName] = React.useState('');
   const [userDescription, setUserDescription] = React.useState('');
   const [userAvatar, setUserAvatar] = React.useState('');
+  const [cards, setCards] = React.useState([]);
 
   React.useEffect(() => {
-    api.getUserData()
-      .then((userData) => {
+    api.getAllData()
+      .then(([userData, cardsData]) => {
         setUserName(userData.name);
         setUserDescription(userData.about);
         setUserAvatar(userData.avatar);
+        setCards(cardsData);
       })
       .catch(err => console.log(err));
   })
@@ -35,10 +38,23 @@ function Main(props) {
 
             <section className="photo-gallery">
                 <ul className="photo-gallery__list">
+                  {cards.map((card) =>
+                    <li key={card._id} className="photo-gallery__item">
+                      <img src={card.link} alt="Фотография загруженная пользователем" className="photo-gallery__image" />
+                      <h2 className="photo-gallery__title">{card.name}</h2>
+                      <div className="photo-gallery__like-section">
+                      <button className="photo-gallery__like-button button-action"></button>
+                      <p className="photo-gallery__like-counter">{card.likes.length}</p>
+                      </div>
+                      <a className="photo-gallery__delete-button button-action"><img
+                      src={trash} alt="Корзина удаления" /></a>
+                    </li>)}
                 </ul>
             </section>
+
         </main>
     );
 }
 
 export default Main;
+
