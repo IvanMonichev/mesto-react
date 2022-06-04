fcdc02824c78b19e32fcd6407ff2a68d5e38bfdc
Ivan Monichev <ivan@monichev.ru>
2022-06-04 23:53:55 +0300

    style: refactoring function isOpen

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,11 +1,6 @@
 function ImagePopup({card, onClose}) {
 
-  const isOpen = () => {
-      if(!(card.link === '')) {
-          return true;
-      }
-      return false;
-  }
+  const isOpen = card.link !== '';
 
   const handleOverlayClose = (event) => {
     if (event.target !== event.currentTarget) {
@@ -16,10 +11,10 @@ function ImagePopup({card, onClose}) {
 
   return (
 
-        <section onClick={handleOverlayClose} className={`popup popup_dark-overlay image-zoom ${isOpen() && 'popup_is-opened'}`}>
+        <section onClick={handleOverlayClose} className={`popup popup_dark-overlay image-zoom ${isOpen && 'popup_is-opened'}`}>
             <div className="image-zoom__container">
                 <figure className="image-zoom__figure">
-                    <img className="image-zoom__image" src={`${isOpen() ? card.link : ""}`}
+                    <img className="image-zoom__image" src={`${isOpen ? card.link : ""}`}
                          alt={`Фотография «${card.name}»`}/>
                     <figcaption className="image-zoom__caption">{card.name}</figcaption>
                 </figure>
